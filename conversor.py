48b47303e7e329ff4ae2cb21affea2f57918cf77
osvaldosc <osvaldosandoval@hotmail.com>
2022-08-04 12:58:18 -0500

    Se modifico elarchivo conversor con una función

--- conversor.py
+++ conversor.py
@@ -1,7 +1,28 @@
-pesos = input("Cuantos pesos tienes?: ")
-pesos = float(pesos)
-valor_dolar = 20
-dolares = pesos / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
-print("Tienes $" + dolares +" dolares")
+def conversor(tipo_pesos, valor_dolar):
+    pesos = input("Cuantos pesos " + tipo_pesos +  "tienes?: ")
+    pesos = float(pesos)
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares +" dolares")
+
+
+menu = """
+Bienvenido al conversor de monedas
+
+1- Pesos Mexicanos
+2- Pesos Colombianos
+3- Pesos Argentinos
+
+Elige una opción: """
+
+opcion = int(input(menu))
+
+if opcion == 1:
+    conversor("Mexicanos", 20)
+elif opcion== 2:
+    conversor("Colombianos", 3875)
+elif opcion == 3:
+    conversor("Argentinos", 65)
+else:
+    print("Escribe una opcion valida")
