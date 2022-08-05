7296022a6afe30bcb0c4cc4f5db00539b9fe3315
osvaldosc <osvaldosandoval@hotmail.com>
2022-08-04 20:09:15 -0500

    Se agrego un ejercicio básico del ciclo for

---
+++ for.py
@@ -0,0 +1,15 @@
+# contador = 1
+# while contador < 1000:
+#     print(contador)
+#     contador = contador +1
+
+# a = list(range(100))
+# print(a)
+
+# for contador in range(1, 1001):
+#     print(contador)
+
+tabla = int(input("Cual tabla quieres multiplicar?: "))
+for i in range(1,11):
+    print(tabla * i)
+    
